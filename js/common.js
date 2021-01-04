637dc16f93fbc90170792f6e12661d92e557f5f1
zhangjing <1047292573@qq.com>
2021-01-04 22:41:33 +0800

    1，添加页面

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ $(function() {
 	$('.dropdown .txt').click(function() {
 		$('.dropdown ul').toggle()
 	});
-	$('.dropdown ul').on('click','li a',function() {
+	$('.dropdown ul').on('click', 'li a', function() {
 		$('.dropdown .txt').text($(this).text());
 		$('.dropdown ul').hide()
 	});
@@ -12,5 +12,21 @@ $(function() {
 		if (!_con.is(event.target) && _con.has(event.target).length === 0) { // Mark 1
 			$('.dropdown ul').hide()
 		}
+	});
+
+	/* 返回顶部 */
+	$('#goToTop').hide(); //隐藏go to top按钮
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$('#goToTop').fadeIn();
+		} else {
+			$('#goToTop').fadeOut();
+		}
+	});
+	$('#goToTop').click(function() {
+		$('html , body').animate({
+			scrollTop: 0
+		}, 'slow');
 	});
 })
